e5e8a6c670f0e7e0f63d2dfcde0efbdc148ee5b3
louzada <hugolouzada@gmail.com>
2016-05-01 12:09:15 -0300

    add partido to feliciano

--- Prediction/SplitTrainValid/getCompleteVoteData.py
+++ Prediction/SplitTrainValid/getCompleteVoteData.py
@@ -1,6 +1,7 @@
 from DataGathering.getNameList import getNameList
 from DataGathering.getVoteData import getVoteData
 from DataTransform import getSpeechSize, getLexicalDiversity, getAverageWordSize, getProperNamePresence
+from sklearn.preprocessing import LabelEncoder
 
 
 def getCompleteVoteData():
@@ -12,6 +13,12 @@ def getCompleteVoteData():
     votes['AverageWordSize'] = getAverageWordSize(votes)
     votes['NamesPresence'] = getProperNamePresence(votes, namesList)
 
+
+    for col in ['Estado','Partido']:
+        votes[col+'_encoded'] = LabelEncoder().fit_transform(votes[col])
+
     votes.drop('Discurso', axis=1, inplace=True)
+    votes.drop('Estado', axis=1, inplace=True)
+    votes.drop('Partido', axis=1, inplace=True)
 
     return votes
